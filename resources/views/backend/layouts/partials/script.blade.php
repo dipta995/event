f61ff8a1da993089cbcef566c91f2980ee0befc3
dipta995 <dipta995@gmail.com>
2022-04-12 00:06:32 +0600

    style script up

--- resources/views/backend/layouts/partials/script.blade.php
+++ resources/views/backend/layouts/partials/script.blade.php
@@ -1,22 +1,34 @@
-<script src="{{ asset('backend/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ asset('backend/assets/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/vendors/perfect-scrollbar/perfect-scrollbar.min.js' }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/js/bootstrap.bundle.min.js' }}"></script>
 
-<script src="{{ asset('backend/assets/vendors/apexcharts/apexcharts.js') }}"></script>
-<script src="{{ asset('backend/assets/js/pages/dashboard.js') }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/vendors/apexcharts/apexcharts.js' }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/js/pages/dashboard.js' }}"></script>
 
 
-<script src="{{ asset('backend/assets/vendors/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('backend/assets/vendors/jquery-datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/vendors/jquery/jquery.min.js' }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/vendors/jquery-datatables/jquery.dataTables.min.js' }}"></script>
 
-<script src="{{ asset('backend/assets/vendors/jquery-datatables/custom.jquery.dataTables.bootstrap5.min.js') }}"></script>
-<script src="{{ asset('backend/assets/vendors/fontawesome/all.min.js') }}"></script>
-<script src="{{ asset('backend/assets/vendors/toastify/toastify.js') }}"></script>
-<script src="{{ asset('backend/assets/js/extensions/toastify.js') }}"></script>
-<script src="{{ asset('backend/assets/vendors/choices.js/choices.min.js') }}"></script>
-<script src="{{ asset('backend/assets/js/pages/form-element-select.js') }}"></script>
-<script src="{{ asset('backend/assets/js/mazer.js') }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/vendors/jquery-datatables/custom.jquery.dataTables.bootstrap5.min.js' }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/vendors/fontawesome/all.min.js' }}"></script>
+
+<script src="{{ ENV('ASSET_LINK').'/vendors/choices.js/choices.min.js' }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/js/pages/form-element-select.js' }}"></script>
+<script src="{{ ENV('ASSET_LINK').'/js/mazer.js' }}"></script>
 <script>
     // Jquery Datatable
-    let jquery_datatable = $("#table1").DataTable()
+    $(document).ready(function() {
+        let jquery_datatable = $("#table1").DataTable();
+               jQuery('#track_id').change(function(){
+                   let track_id = jQuery(this).val();
+                   jQuery.ajax({
+                       url:"{{ url('training_days/') }}/"+track_id,
+                       type:'get',
+                       data:'country_id='+track_id+'&_token={{ csrf_token() }}',
+                       success:function(result){
+                           jQuery('#training_day').html(result)
+                       }
+                   });
+               });
+            });
 </script>
 
--- resources/views/backend/layouts/partials/style.blade.php
+++ resources/views/backend/layouts/partials/style.blade.php
@@ -1,15 +1,15 @@
-<link rel="preconnect" href="https://fonts.gstatic.com">
+{{-- <link rel="preconnect" href="https://fonts.gstatic.com"> --}}
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('backend/assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ ENV('ASSET_LINK').'/css/bootstrap.css' }}">
 
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/iconly/bold.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/choices.js/choices.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/assets/css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/fontawesome/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/toastify/toastify.css') }}">
+    <link rel="stylesheet" href="{{ ENV('ASSET_LINK').'/vendors/iconly/bold.css' }}">
+    <link rel="stylesheet" href="{{ ENV('ASSET_LINK').'/vendors/choices.js/choices.min.css' }}" />
+    <link rel="stylesheet" href="{{ ENV('ASSET_LINK').'/vendors/perfect-scrollbar/perfect-scrollbar.css' }}">
+    <link rel="stylesheet" href="{{ ENV('ASSET_LINK').'/vendors/bootstrap-icons/bootstrap-icons.css' }}">
+    <link rel="stylesheet" href="{{ ENV('ASSET_LINK').'/css/app.css' }}">
+    <link rel="stylesheet" href="{{ ENV('ASSET_LINK').'/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css' }}">
+    <link rel="stylesheet" href="{{ ENV('ASSET_LINK').'/vendors/fontawesome/all.min.css' }}">
+    <link rel="stylesheet" href="{{ ENV('ASSET_LINK').'/vendors/toastify/toastify.css' }}">
     <style>
         table.dataTable td{
             padding: 15px 8px;
